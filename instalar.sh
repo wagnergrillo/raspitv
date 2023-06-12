b9831b8881c53685762ded2d76e0fc1312a5e6c8
wagnergrillo <135171225+wagnergrillo@users.noreply.github.com>
2023-06-12 10:37:31 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -3,3 +3,6 @@ chmod -R 755 LCD-show
 sudo cp kiosk.sh /home/pi
 sudo cp kiosk.service /lib/systemd/system/
 sudo systemctl enable kiosk.service
+sudo cp lightdm.conf /etc/lightdm
+sudo apt-get install unclutter
+unclutter -idle 0.01 -root
